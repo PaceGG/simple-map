85f30c169583abb2040dfd22eafa27914b2fa00a
PaceGG <yura3012004@gmail.com>
2025-10-14 19:14:11 +0500

    feat: Изменение размеров изображениея до фиксированного количества пикселей по ширине

--- src/utils.ts
+++ src/utils.ts
@@ -12,18 +12,18 @@ export const fileToBase64 = async (file: File): Promise<string> => {
     reader.readAsDataURL(file);
   });
 
-  // 🔹 коэффициент сжатия линейный
-  const compressionFactor = 1 / 4.8;
+  const targetWidth = 960;
+  const scaleFactor = targetWidth / img.width;
 
   const canvas = document.createElement("canvas");
-  canvas.width = Math.round(img.width * compressionFactor);
-  canvas.height = Math.round(img.height * compressionFactor);
+  canvas.width = targetWidth;
+  canvas.height = Math.round(img.height * scaleFactor);
 
   const ctx = canvas.getContext("2d");
   if (!ctx) throw new Error("Не удалось получить 2D контекст");
 
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-  // Получаем Base64 в JPEG с качеством 0.7
-  return canvas.toDataURL("image/jpeg", 0.7);
+  // Получаем Base64 в WebP с качеством 0.7
+  return canvas.toDataURL("image/webp", 1);
 };
